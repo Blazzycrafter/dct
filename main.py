e1199b54f794c24be630b2e867445cf88f617ad7
Blazzycrafter <>
2021-04-25 02:35:48 +0000

    Adding SRC COMMAND

--- main.py
+++ main.py
@@ -2,5 +2,33 @@ import os
 import discord
 
 
+
+SRC = 'LINK'
+PREFIX = '*'
+
+
 DCToken = os.environ['TOKEN']
-print()
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as User: {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    print('ME: '+ message.content)
+    return
+  else:
+    print(str(message.author) +': '+ message.content)
+  
+  if message.content == f'{PREFIX}src':
+    await message.channel.send(f'SRC: {SRC}')
+
+
+
+
+
+client.run(DCToken)
