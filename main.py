2d570ae6487851f9706b3ededa64dbb235e4780d
Blazzycrafter <>
2021-05-27 18:09:51 +0000

    build ready now try desing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 from keep_alive import keep_alive
 import requests
+import json
 #keep_alive Start
 keep_alive()
 
@@ -9,6 +10,8 @@ SRC = 'https://github.com/Blazzycrafter/dct/'
 PREFIX = '*'
 
 
+
+Guild_id = os.environ['Guild_id']
 
 BT = os.environ['UnbelevToken']
 DCToken = os.environ['TOKEN']
@@ -39,7 +42,7 @@ async def on_message(message):
 
     data = '{ "cash": 10}'
 
-    response = requests.patch(f'https://unbelievaboat.com/api/v1/guilds/305129477627969547/users/{message.author.id}', headers=headers, data=data)
+    response = requests.patch(f'https://unbelievaboat.com/api/v1/guilds/{Guild_id}/users/{message.author.id}', headers=headers, data=data)
     await message.channel.send(response.text)
 
 
